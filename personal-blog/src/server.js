fe6fddcb5d6a0472c11f5e8f6b434194a5e05bd6
Le Hong Phuc <lehongphucnasa@gmail.com>
2026-05-24 10:54:41 +0700

    add styling

--- personal-blog/src/server.js
+++ personal-blog/src/server.js
@@ -16,6 +16,11 @@ const {
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(
+    express.static(
+        path.join(__dirname, "public")
+    )
+);
+app.use(
     session({
         secret: process.env.SESSION_SECRET,
         resave: false,
